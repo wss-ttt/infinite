737e3e6de0cadf84657c68bc46d7f0d63d43b007
王水胜 <602466011@qq.com>
2019-08-29 14:49:04 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: resovle => require(['@/views/index.vue'], resovle)
+    },
+    {
+      path:'/index2',
+      name:'index2',
+      component: resovle => require(['@/views/index2.vue'], resovle)
+    }
+  ]
+})
---
+++ src/views/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <ul  v-infinite-scroll="load" style="overflow:auto">
+      <li v-for="i in count" :key="i">{{ i }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      count: 0
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    load() {
+    console.log('load')
+      this.count += 1
+    }
+  },
+  created() {},
+  mounted() {}
+}
+</script>
+<style scoped>
+ul{
+    background-color: #ccc;
+    height: 300px;
+}
+ul li{
+    height: 20px;
+    margin: 10px;
+    background-color: #1acd7e;
+}
+</style>
